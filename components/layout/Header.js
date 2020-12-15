a86148bdd1715570d6d1f5e0621ee27b4a693ae9
unknown <alextello1@hotmail.com>
2020-12-15 12:31:02 -0600

    maquetacion base

---
+++ components/layout/Header.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import Buscar from '../UI/Buscar';
+import Navegacion from './Navegacion';
+import Link from 'next/Link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import Boton from '../UI/Boton';
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px){
+        display: flex;
+        justify-content: space-between;
+     }
+`;
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+    cursor: pointer;
+`;
+
+const Header = () => {
+
+    const usuario = false;
+
+    return (
+        <header
+            css={css`
+            border-bottom: 2px solid var(--gris3);
+            padding: 1rem 0;
+        `}>
+            <ContenedorHeader>
+                <div css={
+                    css`
+                        display: flex;
+                        align-items: center;
+                    `
+                }>
+                    <Link href="/">
+                        <Logo>p</Logo>
+                    </Link>
+                    <Buscar />
+                    <Navegacion />
+                </div>
+                <div css={css`
+                    display: flex;
+                    align-items: center;
+                `}>
+                    {usuario ? (
+                        <>
+                            <p css={css`
+                            margin-right: 2rem;
+                        `}>Hola: Alex</p>
+                            <Boton bgColor="true">Cerrar sesion</Boton>
+                        </>
+                    ) :
+                        (
+                            <>
+                                <Link href="/login">
+                                    <Boton bgColor="true">
+                                        Login
+                                    </Boton>
+                                </Link>
+                                <Link href="/crear-cuenta">
+                                    <Boton>
+                                        Crear cuenta
+                                    </Boton>
+                                </Link>
+                            </>
+                        )
+                    }
+                </div>
+            </ContenedorHeader>
+        </header>
+    );
+}
+
+export default Header;
---
+++ pages/buscar.js
@@ -0,0 +1,14 @@
+import Layout from '../components/layout/Layout';
+
+const Buscar = () => {
+    return (
+        <div>
+            <Layout>
+                <h1>Crear cuenta</h1>
+            </Layout>
+
+        </div>
+    );
+}
+
+export default Buscar;
